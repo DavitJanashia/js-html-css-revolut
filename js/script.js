a4a92e32d4ba5db8b5d4ed2244a4520fdfe09dc2
Davit <davit.janashia1@gmail.com>
2020-07-06 16:57:13 +0200

    iniziato jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // fare 2 menu diverse?? o cercare su internet come gestire responsive?
+$(document).ready(
+  function(){
+    $('.menu > li').mouseenter(
+      function(){
+        console.log('ciao');
+      }
+
+    );
+  }
+);
