bec1031710e16e53772b633d055725ec676d74ad
Davit <davit.janashia1@gmail.com>
2020-07-07 12:08:47 +0200

    fatto 2 nav menu: uno normale altro con h.m. responsive da finire

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // fare 2 menu diverse?? o cercare su internet come gestire responsive?
 // enter/leave menu
-
+$(document).ready(function() {
 $('.parent-dropdown')
   .mouseenter(function() {
     $(this).children('.dropdown').show();
@@ -18,3 +18,15 @@ $('.parent-dropdown')
 //   });
 //
 // });
+
+
+  $('.hamburger-icon').click(function() {
+    // $(this).children('.dropdown').toggleClass('hide, show');
+    $('.hamburger-menu').removeClass('hide').addClass('show');
+  });
+
+  $('#my-close').click(function(){
+    $('.hamburger-menu').removeClass('show').addClass('hide');
+  });
+
+});
